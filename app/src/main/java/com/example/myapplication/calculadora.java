39cfa95b27d40294c480428562e24de69f0c26a3
lab <user@IFPR-Lab.localdomain>
2023-03-07 21:13:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/calculadora.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class calculadora extends AppCompatActivity {otected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+    }
+}
